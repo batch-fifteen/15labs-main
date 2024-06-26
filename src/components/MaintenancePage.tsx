90bb880020daeea9c3fb64802eb831a36903be8b
Afan Dayu Laksono <afandayul@gmail.com>
2024-06-27 03:46:33 +0700

    adding maintenance page

---
+++ src/components/MaintenancePage.tsx
@@ -0,0 +1,18 @@
+const MaintenancePage = () => {
+  return (
+    <article>
+      <h1>We’ll be back soon!</h1>
+      <div className="mx-[300px]">
+        <p className="text-center text-lg">
+          Sorry for the inconvenience but we’re performing some maintenance at
+          the moment. If you need to you can always{" "}
+          <a href="mailto:information.15labs@gmail.com">contact us</a>,
+          otherwise we’ll be back online shortly!
+        </p>
+        <p>— The Team</p>
+      </div>
+    </article>
+  );
+};
+
+export default MaintenancePage;
