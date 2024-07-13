9d468d440f3ac7776c7b0d285c7b077bb2da72ca
Afan Dayu Laksono <afandayul@gmail.com>
2024-07-13 22:22:32 +0700

    feat: view home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
-import MaintenancePage from "./components/MaintenancePage";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import MaintenancePage from "./components/MaintenancePage";
+import HomeComponent from "./components/home/HomeComponent";
 
 function App() {
-  return <MaintenancePage />;
+  return (
+    <>
+      <div className="bg-[#151D48] h-screen">
+        <HomeComponent />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/home/HomeComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CustomCarouselComponent } from "./CustomCarouselComponent";
+
+const HomeComponent: React.FC = () => {
+  return (
+    <div className="">
+      <CustomCarouselComponent />
+    </div>
+  );
+};
+
+export default HomeComponent;
