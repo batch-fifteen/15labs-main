5b74fb98f6a0210b81384708be0071517acf850b
gagaswahyuin <gagaswahyuin@gmail.com>
2024-07-26 22:28:23 +0700

    feat: add contact us

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import HomeComponent from "./components/home/HomeComponent";
 import Portfolio from "./components/portfolio";
+import ContactUs from "./components/contact-us/ContactUs";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <HomeComponent />
       <AboutUs />
       <Portfolio />
+      <ContactUs />
       <Footer />
     </div>
   );
