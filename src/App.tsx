99cabe55283e63e008bda47804e83e566bdf889d
gagaswahyuin <gagaswahyuin@gmail.com>
2024-07-29 21:49:18 +0700

    feat: add anchorlink

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,32 @@ import "slick-carousel/slick/slick-theme.css";
 import HomeComponent from "./components/home/HomeComponent";
 import Portfolio from "./components/portfolio";
 import ContactUs from "./components/contact-us/ContactUs";
+import { useRef } from "react";
 
 function App() {
+  const homeRef = useRef<HTMLDivElement | null>(null);
+  const aboutUsRef = useRef<HTMLDivElement | null>(null);
+  const worksRef = useRef<HTMLDivElement | null>(null);
+  const contactRef = useRef<HTMLDivElement | null>(null);
   return (
     <div>
-      <Header />
+      <Header
+        homeRef={homeRef}
+        aboutUsRef={aboutUsRef}
+        worksRef={worksRef}
+        contactRef={contactRef}
+      />
       <HomeComponent />
-      <AboutUs />
-      <Portfolio />
-      <ContactUs />
+      <div ref={aboutUsRef}>
+        <AboutUs />
+      </div>
+      <div ref={worksRef}>
+        <Portfolio />
+      </div>
+      <div ref={contactRef}>
+        <ContactUs />
+      </div>
+
       <Footer />
     </div>
   );
