ca18f6321607add8f64959e90bc121b40ccf2cc6
Afan Dayu Laksono <afandayul@gmail.com>
2024-08-11 19:12:33 +0700

    feat: adding mobile view for about us

--- src/components/about-us/AboutUsContent.tsx
+++ src/components/about-us/AboutUsContent.tsx
@@ -6,53 +6,60 @@ import "./AboutUsContent.css";
 
 const AboutUsContent: React.FC = () => {
   return (
-    <section className="relative px-12">
-      <div className="w-[213px] h-[193px] bg-[#EEEEEE7A] absolute top-40 left-0 -z-10"></div>
-      <div className="w-[213px] h-[193px] bg-[#EEEEEE7A] absolute bottom-0 right-0 -z-10"></div>
+    <section className="relative px-4 md:px-12">
+      <div className="w-[150px] h-[140px] bg-[#EEEEEE7A] absolute bottom-0 right-0 -z-10 hidden md:block"></div>
       {/* <!-- Software Engineering Practices Section --> */}
       <div className="container mx-auto py-8">
         <div className="mt-8 mb-10 text-center">
-          <h1 className="text-4xl font-bold mb-4">
+          <h1 className="text-3xl md:text-4xl font-bold mb-4">
             Our Software Engineering Practices
           </h1>
-          <div className="w-[32rem] mx-auto">
-            <p className="mb-6 text-lg leading-[1.318rem]">
+          <div className="w-full md:w-[32rem] mx-auto">
+            <p className="mb-6 text-base md:text-lg leading-relaxed">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.
             </p>
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-          <div className="flex flex-col gap-24">
+          <div className="flex flex-col gap-8">
             <div className="flex flex-col mb-6 p-6 shadow-lg bg-[#FFFFFF] relative">
               <div className="decagon h-24 w-24 p-5 bg-blue-700 flex justify-center mb-2 absolute -top-10">
-                <img src={ReactSVG} alt="React" />
+                <img src={ReactSVG} alt="React" className="w-12 h-12" />
               </div>
-              <h2 className="text-xl font-semibold mb-4 mt-10 text-start">
+              <h2 className="text-lg md:text-xl font-semibold mb-4 mt-10 text-start">
                 Web Development
               </h2>
-              <p className="text-justify mb-6">
+              <p className="text-justify mb-6 text-sm md:text-base">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s,
+                text ever since the 1500s.
               </p>
             </div>
             <div className="flex flex-col mb-6 p-6 shadow-lg bg-[#FFFFFF] relative">
               <div className="decagon h-24 w-24 p-5 bg-blue-700 flex justify-center mb-2 absolute -top-10">
-                <img className="rotate-90" src={AndroidSVG} alt="Android" />
+                <img
+                  className="rotate-90 w-12 h-12"
+                  src={AndroidSVG}
+                  alt="Android"
+                />
               </div>
-              <h2 className="text-xl font-semibold mb-4 mt-10 text-start">
+              <h2 className="text-lg md:text-xl font-semibold mb-4 mt-10 text-start">
                 Android Development
               </h2>
-              <p className="text-justify">
+              <p className="text-justify text-sm md:text-base">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s,
+                text ever since the 1500s.
               </p>
             </div>
           </div>
           <div className="flex justify-center items-center">
-            <img src={Code} alt="Code" />
+            <img
+              src={Code}
+              alt="Code"
+              className="hidden md:block w-full md:w-auto"
+            />
           </div>
         </div>
       </div>
--- src/components/about-us/AboutUsHeader.tsx
+++ src/components/about-us/AboutUsHeader.tsx
@@ -5,35 +5,40 @@ import ShieldSVG from "../../assets/Shield.svg";
 
 const AboutUsHeader: React.FC = () => {
   return (
-    <section className="bg-half-half w-full h-56 px-12">
-      <div className="container mx-auto flex flex-wrap md:flex-nowrap items-center gap-4 h-full">
-        <div className="w-full md:w-1/3 pe-8 text-justify py-6">
-          <h3 className="text-xl font-semibold mb-4">About Us</h3>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s,
-          </p>
-        </div>
-
-        <div className="flex w-full md:w-2/3 h-full">
-          <div className="bg-[#335EF7] flex flex-col items-center p-4 h-full w-full justify-center">
-            <div className="bg-white bg-opacity-50 w-20 h-20 rounded-full flex justify-center items-center mb-2">
-              <img className="w-14" src={LaptopSVG} alt="Laptop" />
-            </div>
-            <h5 className="text-white font-semibold">Professional</h5>
+    <section className="w-full h-auto md:pl-12">
+      <div className=" grid grid-cols-1 md:grid-cols-4 items-center">
+        <div className="grid grid-cols-1  md:col-span-4 md:grid-cols-4">
+          <div className="text-center md:text-left py-10">
+            <h3 className="text-2xl md:text-xl font-semibold mb-4">About Us</h3>
+            <p className="text-base md:text-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s.
+            </p>
           </div>
-          <div className="bg-[#167EF8] flex flex-col items-center p-4 h-full w-full justify-center">
-            <div className="bg-white bg-opacity-50 w-20 h-20 rounded-full flex justify-center items-center mb-2">
+          <div className="bg-[#335EF7] flex flex-col items-center p-4 w-full h-full justify-center">
+            <div className="bg-white bg-opacity-50 w-16 h-16 rounded-full flex justify-center items-center mb-2">
+              <img className="w-12" src={LaptopSVG} alt="Laptop" />
+            </div>
+            <h5 className="text-white font-semibold text-sm md:text-base">
+              Professional
+            </h5>
+          </div>
+          <div className="bg-[#167EF8] flex flex-col items-center p-4 w-full h-full justify-center">
+            <div className="bg-white bg-opacity-50 w-16 h-16 rounded-full flex justify-center items-center mb-2">
               <img className="w-12" src={RocketSVG} alt="Rocket" />
             </div>
-            <h5 className="text-white font-semibold">Professional</h5>
+            <h5 className="text-white font-semibold text-sm md:text-base">
+              Professional
+            </h5>
           </div>
-          <div className="bg-[#33D4F7] flex flex-col items-center p-4 h-full w-full justify-center">
-            <div className="bg-white bg-opacity-50 w-20 h-20 rounded-full flex justify-center items-center mb-2">
-              <img className="w-14" src={ShieldSVG} alt="Shield" />
+          <div className="bg-[#33D4F7] flex flex-col items-center p-4 w-full h-full justify-center">
+            <div className="bg-white bg-opacity-50 w-16 h-16 rounded-full flex justify-center items-center mb-2">
+              <img className="w-12" src={ShieldSVG} alt="Shield" />
             </div>
-            <h5 className="text-white font-semibold">Professional</h5>
+            <h5 className="text-white font-semibold text-sm md:text-base">
+              Professional
+            </h5>
           </div>
         </div>
       </div>
