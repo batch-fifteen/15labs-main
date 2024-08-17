907251b1eccf030707edb87c4f6e7d26755561d6
Afan Dayu Laksono <afandayul@gmail.com>
2024-08-17 16:05:05 +0700

    feat: change port

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080, // Set the port to 8080
+    host: '0.0.0.0' // Expose to network if needed
+  }
 })
