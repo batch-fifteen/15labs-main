c53d22aa06cf9b6a73f7f34c79b78ec26fc46b57
Afan Dayu Laksono <afandayul@gmail.com>
2024-08-18 23:31:17 +0700

    try deploy

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,12 @@ import ContactUs from "./components/contact-us/ContactUs";
 import { useRef } from "react";
 
 function App() {
+
   const homeRef = useRef<HTMLDivElement | null>(null);
   const aboutUsRef = useRef<HTMLDivElement | null>(null);
   const worksRef = useRef<HTMLDivElement | null>(null);
   const contactRef = useRef<HTMLDivElement | null>(null);
+  
   return (
     <div>
       <Header
