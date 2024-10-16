d4c3c1888ee76586c0b73f338fb6762e4f154ff5
Afan Dayu Laksono <afandayul@gmail.com>
2024-10-17 00:57:48 +0700

    fix: show chevron

--- src/components/floating-button/floating-button.tsx
+++ src/components/floating-button/floating-button.tsx
@@ -1,50 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import FloatingImage from '../../assets/floatingbutton.svg';
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronCircleDown,
+  faChevronCircleUp,
+  faChevronUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 interface FloatingButtonProps {
-    homeRef: React.RefObject<HTMLDivElement>;
+  homeRef: React.RefObject<HTMLDivElement>;
 }
 const FloatingButton: React.FC<FloatingButtonProps> = ({ homeRef }) => {
-    const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
-    const handleScroll = () => {
-        if (window.scrollY > 100) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
-    };
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-    const buttonStyle = {
-        position: 'fixed' as 'fixed',
-        bottom: '20px',
-        right: '20px',
-        backgroundColor: '#151D48',
-        color: 'white',
-        border: 'none',
-        borderRadius: '20%',
-        padding: '20px',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)',
-        cursor: 'pointer',
-        transition: 'background-color 0.3s',
-        display: isVisible ? 'block' : 'none'
-    };
+  const buttonStyle = {
+    position: "fixed" as "fixed",
+    bottom: "20px",
+    right: "20px",
+    backgroundColor: "#151D48",
+    border: "none",
+    borderRadius: "20%",
+    padding: "15px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)",
+    cursor: "pointer",
+    transition: "background-color 0.3s",
+    display: isVisible ? "block" : "none",
+  };
 
-    const buttonHoverStyle = {
-        backgroundColor: '#45a049',
-    };
-
-    return (
-        
-        <div ref={homeRef} style={buttonStyle}>
-            <img src={FloatingImage} alt="Floating Button"/>
-        </div>
-    );
-}
+  return (
+    <div style={buttonStyle}>
+      <FontAwesomeIcon icon={faChevronUp} color="white" />
+    </div>
+  );
+};
 
 export default FloatingButton;
